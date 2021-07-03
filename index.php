472354fcbdb30f683749d743a3fe9a62905168ed
Very Gois <sorryforthedelayinsending@vg.pe.hu>
2021-07-04 05:37:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
+<?php
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+function h($str) {
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+$text = (string)filter_input(INPUT_POST, 'text'); // $_POST['text']
+$name = (string)filter_input(INPUT_POST, 'name'); // $_POST['name']
+
+$fp = fopen('list.csv', 'a+b');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    flock($fp, LOCK_EX);
+    fputcsv($fp, [$text, $name]);
+    rewind($fp);
+}
+
+flock($fp, LOCK_SH);
+while ($row = fgetcsv($fp)) {
+    $rows[] = $row;
+}
+flock($fp, LOCK_UN);
+fclose($fp);
+?>
+<!DOCTYPE html>
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width">
@@ -41,79 +62,12 @@ $(function(){
 
 <ul id="s3" class="es">
 <span>ABOUT</span>
-<li>弘法筆を選ばず</li>
-<li>早起きは三文の得</li>
-<li>質実剛健</li>
-<li>Harder Better Faster Stronger</li>
-<li>山あり谷あり</li>
-<li>落ち着いたらできる</li>
-<li>バカの壁   (by養老孟司)</li>
-<li>世界平和</li>
-<li>心にいつもヤンキーを</li>
-<li>Poco a Poco</li>
-<li>誰にでもできるし、もうできてる</li>
-<li>永久の未完成、それすなわち完成である</li>
-<li>安物買いの銭失い</li>
-<li>なるようになる</li>
-<li>何事も全力投球</li>
-<li>健康に気をつける</li>
-<li>Being into something</li>
-<li>努力の鬼</li>
-<li>感謝</li>
-<li>誠実</li>
-<li>前向き</li>
-<li>たのしいを見つける</li>
-<li>着の身着のまま</li>
-<li>常にぷにぷにの心であれ</li>
-<li>yes we can </li>
-<li>super original</li>
-<li>質実剛健</li>
-<li>ていのうきもちわるい</li>
-<li>モジモジせずに大胆に</li>
-<li>役立たずの夜の灯台</li>
-<li>Yes, we can</li>
-<li>野良犬であれ</li>
-<li>右の頬を叩かれたら、左の頬も出しなさい。</li>
-<li>ゆくてを塞ぐ邪魔な石を蟾蜍は廻って通る。</li>
-<li>隠れて生きよ</li>
-<li>ながれにさからえ</li>
-<li>待てば海路の日和あり</li>
-<li>I&#039;ll fuck anything that moves!</li>
-<li>健康一番</li>
-<li>エリクサーは躊躇わずに使え</li>
-<li>フォースと共にあれ</li>
-<li>機械のほうが20%今年ももう終わりだ</li>
-<li>調理を脱構築すればいいと思うの</li>
-<li>無言実行</li>
-<li>GREEN THOUGHTS</li>
-<li>腹八分</li>
-<li>失敗は成功のもと</li>
-<li>人は考える葦である</li>
-<li>性格は変えられないけど、生き方は変えられる</li>
-<li>8月10日生まれの性格を降臨させる</li>
-<li>I&#039;m just a little person</li>
-<li>吾輩は猫である</li>
-<li>できることをする、できないことはできない</li>
-<li>食べないなら太ったほうがマシ</li>
-<li>待てば海路の日和あり</li>
-<li>終わりよければ全てよし</li>
-<li>あとは野となれ山となれ</li>
-<li>私は真実を知っている</li>
-<li>座らなければ家事は終わる</li>
-<li>エゴのないバンドマンはクソ</li>
-<li>さよならだけが人生</li>
-<li>定説です</li>
-<li>脛を齧るなら骨髄まで</li>
-<li>無し</li>
-<li>他言無用</li>
-<li>刹那的に生きる</li>
-<li>あると思います</li>
-<li>なるようにしかならない</li>
-<li>私のお墓の前で泣かないでください</li>
-<li>転んでもタダでは起きない</li>
-<li>論より証拠</li>
-<li>論より証拠</li>
-<li>論より証拠</li>
+<?php if (!empty($rows)): ?>
+<?php foreach ($rows as $row): ?>
+<li><?=h($row[0])?></li>
+<?php endforeach; ?>
+<?php else: ?>
+<?php endif; ?>
 </ul>
 </div>
 </div>
